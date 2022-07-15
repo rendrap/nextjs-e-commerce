3c097912388c5918eddc19c33fc2be0380886b6b
rendrap <rendrapradana@gmail.com>
2022-07-15 11:06:32 +0700

    add data.js

---
+++ utils/data.js
@@ -0,0 +1,15 @@
+const data = {
+  products: [
+    {
+      name: 'Free Shirt',
+      category: 'Shirt',
+      image: '/images/shirt1.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: 'A popular shirt',
+    },
+  ],
+};
